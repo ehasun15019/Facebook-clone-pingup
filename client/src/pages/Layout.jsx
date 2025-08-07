f97c3b71a5f359f972a70b881fa191d0b301d6b7
Your Full Name <ehasun0909@gamil.com>
2025-08-08 00:09:29 +0600

    design the Layout.jsx page and create sideBar component and impelment the Layout.jsx

--- client/src/pages/Layout.jsx
+++ client/src/pages/Layout.jsx
@@ -1,9 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
+import SideBar from "../components/SideBar";
+import { Outlet } from "react-router-dom";
+import { Flag, Menu, X } from "lucide-react";
 
 const Layout = () => {
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+
   return (
-    <div>
-      <p>Layout</p>
+    <div className="w-full flex h-screen">
+      <SideBar />
+
+      <div className="flex-1 bg-slate-50">
+        <Outlet />
+      </div>
+
+      {sidebarOpen ? (
+        <X
+          className="absolute top-3 right-3 p-2 z-100 bg-white rounded-md shadow w-10 h-10 text-gray-600 sm:hidden"
+          onClick={() => setSidebarOpen(false)}
+        />
+      ) : (
+        <Menu
+          className="absolute top-3 right-3 p-2 z-100 bg-white rounded-md shadow w-10 h-10 text-gray-600 sm:hidden"
+          onClick={() => setSidebarOpen(true)}
+        />
+      )}
     </div>
   );
 };
