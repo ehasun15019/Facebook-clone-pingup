bc56d6e08b7972d13942dc8836ab79fb82971f8d
Your Full Name <ehasun0909@gamil.com>
2025-08-08 00:39:08 +0600

    update Sidebar.jsx the sidebarOpen & setSidebarOpen comes form Layout.jsx. Because it is convert by props

--- client/src/pages/Layout.jsx
+++ client/src/pages/Layout.jsx
@@ -11,7 +11,7 @@ const Layout = () => {
 
   return user ? (
     <div className="w-full flex h-screen">
-      <SideBar />
+      <SideBar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
 
       <div className="flex-1 bg-slate-50">
         <Outlet />
