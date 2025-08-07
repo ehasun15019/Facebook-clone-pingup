4a43d20f5d5fd0b73009ce242ba1208b045be8c4
Your Full Name <ehasun0909@gamil.com>
2025-08-07 18:45:06 +0600

    setup pages folder in client

---
+++ client/src/pages/Connections.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Connections = () => {
+  return (
+    <div>
+      <p>Connections</p>
+    </div>
+  );
+};
+
+export default Connections;
---
+++ client/src/pages/CreatePost.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const CreatePost = () => {
+  return (
+    <div>
+      <p>CreatePost</p>
+    </div>
+  );
+};
+
+export default CreatePost;
