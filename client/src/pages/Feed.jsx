8b731ef7f16ac5df41e5439ac2f83750dd4194f5
Your Full Name <ehasun0909@gamil.com>
2025-08-08 12:23:59 +0600

    start to update Feed.jsx page

--- client/src/pages/Feed.jsx
+++ client/src/pages/Feed.jsx
@@ -1,10 +1,40 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { dummyPostsData } from "../assets/assets";
+import Loading from "../components/Loading";
 
 const Feed = () => {
-  return (
-    <div>
-      <p>Feed</p>
+  const [feeds, setFeeds] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const fetchFeeds = async () => {
+    setFeeds(dummyPostsData);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    fetchFeeds();
+  }, []);
+
+  return !loading ? (
+    <div className="h-full overflow-y-scroll no-scrollbar py-10 xl:pr-5 flex items-start justify-center xl:gap-8">
+      {/* stories and post list */}
+      <div>
+        <h1>Stories here</h1>
+
+        <div className="py-4 space-y-6">List post</div>
+      </div>
+
+      {/* Right sidebar */}
+      <div>
+        <div>
+          <h1>Sponsored</h1>
+        </div>
+
+        <h1>Recent Message</h1>
+      </div>
     </div>
+  ) : (
+    <Loading />
   );
 };
 
