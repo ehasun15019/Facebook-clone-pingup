f142c200fc1841f40e89dc725841b8db80bf5148
Your Full Name <ehasun0909@gamil.com>
2025-08-12 02:01:46 +0600

    create StoriesModal component and import in StoriesBar.jsx component

--- client/src/components/StoriesBar.jsx
+++ client/src/components/StoriesBar.jsx
@@ -2,9 +2,12 @@ import React, { useEffect, useState } from "react";
 import { dummyStoriesData } from "../assets/assets";
 import { Plus } from "lucide-react";
 import moment from "moment";
+import StoriesModal from "./StoriesModal";
 
 const StoriesBar = () => {
   const [stories, setStories] = useState([]);
+  const [showModal, setShowModal] = useState(false);
+  const [viewStory, setViewStory] = useState(null);
 
   const fetStories = async () => {
     setStories(dummyStoriesData);
@@ -18,7 +21,10 @@ const StoriesBar = () => {
     <div className="w-screen sm:w-[calc(100vw-240px)] lg:max-w-2xl no-scrollbar overflow-x-auto px-4">
       <div className="flex gap-4 pb-5">
         {/* Add Stories card */}
-        <div className="rounded-lg shadow-sm min-w-[120px] max-w-[120px] max-h-[160px] aspect-[3/4] cursor-pointer hover:shadow-lg transition-all duration-200 bg-gradient-to-b from-indigo-50 to-white">
+        <div
+          onClick={() => setShowModal(true)}
+          className="rounded-lg shadow-sm min-w-[120px] max-w-[120px] max-h-[160px] aspect-[3/4] cursor-pointer hover:shadow-lg transition-all duration-200 bg-gradient-to-b from-indigo-50 to-white"
+        >
           <div className="h-full flex flex-col items-center justify-center p-4">
             <div className="size-10 bg-indigo-500 rounded-full flex items-center justify-center mb-3">
               <Plus className="w-5 h-5 text-white" />
@@ -68,6 +74,11 @@ const StoriesBar = () => {
           </div>
         ))}
       </div>
+
+      {/* add story modal */}
+      {showModal && (
+        <StoriesModal setShowModal={setShowModal} fetchStories={fetStories} />
+      )}
     </div>
   );
 };
